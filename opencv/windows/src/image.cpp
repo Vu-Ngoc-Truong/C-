88c3604dbb42138ece1a9370297588c1f4f08623
ngoctruongns <vungoctruong8x@gmail.com>
2024-02-24 10:22:20 +0700

    cmake opencv for window

---
+++ opencv/windows/src/image.cpp
@@ -0,0 +1,25 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+int main()
+{
+    std::string image_path = samples::findFile("starry_night.jpg");
+    Mat img = cv::imread(image_path, cv::IMREAD_COLOR);
+    if (img.empty())
+    {
+        std::cout << "Could not read the image:" << image_path << std::endl;
+        return 1;
+    }
+
+    imshow("Display Window", img);
+    int k = waitKey(0);
+    if (k == 's')
+    {
+        imwrite("starry_night.png", img);
+    }
+
+    return 0;
+}
